b686115097733015bea8185359f97ad3022edd5b
paubric <paubric@gmail.com>
2019-11-09 16:16:49 +0100

    Update site

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from google_wrapper import get_entity_sentiment
 
 def jacobs(request):
+    # Set CORS headers for the preflight request
+    if request.method == 'OPTIONS':
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+        }
+
+        return ('', 204, headers)
+
+    # Set CORS headers for the main request
+    headers = {
+        'Access-Control-Allow-Origin': '*'
+    }
+
     query = request.args.get('query')
-    analysis = get_entity_sentiment(query)
+    handle = getHandle(query)
+    tweets = getTweets(handle)
+    analysis = get_entity_sentiment(tweets)
 
-    return analysis
-
+    return (analysis, 200, headers)
+    
 if __name__ == '__main__':
     app.run()
