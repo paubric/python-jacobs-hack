5bad5e3a3aeaaab53eb922adf7926f0fa8bff144
paubric <paubric@gmail.com>
2019-11-09 11:31:35 +0100

    Update twitter_wrapper.py

---
+++ twitter_wrapper.py
@@ -0,0 +1,7 @@
+# Take in "United Nations" and return "UN" (its Twitter handle)
+def getHandle(query):
+    pass
+
+# Take in "UN" and return ['last tweet', 'previous tweet']
+def getTweets(handle):
+    pass
